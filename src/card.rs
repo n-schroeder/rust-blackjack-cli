4f4aba2ba99280e0a7fdfc3d03bdd0138caf55a4
Noah Schroeder <noah.schroeder@gmail.com>
2025-12-11 20:59:59 -0700

    feat: Add Suit and Rank Enums

---
+++ src/card.rs
@@ -0,0 +1,22 @@
+pub enum Suit {
+    Hearts,
+    Diamonds,
+    Clubs,
+    Spades,
+}
+
+pub enum Rank {
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE,
+}
