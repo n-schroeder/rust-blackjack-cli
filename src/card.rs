cd3d473ce501eb6925915c90bcab296c15f61dac
Noah Schroeder <noah.schroeder@gmail.com>
2025-12-11 21:23:05 -0700

    feat: create value function to match Rank enum with Card value

--- src/card.rs
+++ src/card.rs
@@ -1,3 +1,4 @@
+// Create Suit enum
 pub enum Suit {
     Hearts,
     Diamonds,
@@ -5,6 +6,7 @@ pub enum Suit {
     Spades,
 }
 
+// Create Rank enum
 pub enum Rank {
     TWO,
     THREE,
@@ -21,7 +23,29 @@ pub enum Rank {
     ACE,
 }
 
+// Create Card Struct
 struct Card {
     suit: Suit,
     rank: Rank,
+}
+
+// Define Card implications
+impl Card {
+    pub fn value(&self) -> u8 {
+        // match enum with Rank value
+        match self {
+            Rank::TWO => 2,
+            Rank::THREE => 3,
+            Rank::FOUR => 4,
+            Rank::FIVE => 5,
+            Rank::SIX => 6,
+            Rank::EIGHT => 8,
+            Rank::NINE => 9,
+            Rank::TEN => 10,
+            Rank::JACK => 10,
+            Rank::QUEEN => 10,
+            Rank::KING => 10,
+            Rank::ACE => 11,
+        }
+    }
 }
