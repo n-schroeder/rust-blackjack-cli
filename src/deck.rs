0459e789657af97211d14cbd7760182aace7d52a
Noah Schroeder <noah.schroeder@gmail.com>
2025-12-16 15:03:38 -0700

    docs(deck): add comments to clarify code methodology

--- src/deck.rs
+++ src/deck.rs
@@ -23,14 +23,25 @@ use rand::Rng;
         // return cards as Deck using field init shorthand
         Deck { cards }
     }
+
+    // shuffle deck method
     pub fn shuffle(&mut self) {
+        // initialize rng as a range
         let mut rng = rand::rng();
+
+        // use for loop to reverse iterate through Deck.cards
         for i in (1..self.cards.len()).rev() {
+            // n = random number in range 0 through current i iteration
             let n = rng.random_range(0..=i);
+
+            // swap random index with current index
             self.cards.swap(i,n);
         }
     }
+
+    // deal from deck method
     pub fn deal(&mut self) -> Option<Card> {
+        // using pop() returns a wrapped top Card and removes that Card from cards
         self.cards.pop()
     }
  }
@@ -49,25 +60,41 @@ mod tests {
         assert_eq!(d.cards.len(), 52);
     }
 
+    // test shuffle method
     # [test]
     fn test_deck_shuffle() {
+        // create new deck
         let mut d = Deck::new();
+        // print new deck
         for i in 0..52 {
             println!("Item {}: {:?}", i + 1, d.cards[i]);
         }
         println!();
+        
+        // shuffle new deck
         d.shuffle();
+
+        // print new deck
         for i in 0..52 {
             println!("Item {}: {:?}", i + 1, d.cards[i]);
         }
+
+        // NOTE:
+
+        // must manually test using cargo test -- --nocapture
     }
 
+    // test deal card method
     # [test]
     fn test_deal() {
+        // create two different Card objects,
+        // one grabbing Card straight from index 51
+        // and the other grabbing the wrapped return card from .deal()
         let mut d = Deck::new();
         let card = d.cards[51];
         let return_card = d.deal();
         
+        // assert that cards are equivalent after unwrapping return card
         assert_eq!(card, return_card.unwrap())
     }
 }
