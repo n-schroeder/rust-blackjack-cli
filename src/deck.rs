334bf4ffb63e3d580fc59bea1fa69a57d33db2db
Noah Schroeder <noah.schroeder@gmail.com>
2025-12-15 23:27:37 -0700

    feat(deck): implement constructor to initialize 52 cards

--- src/deck.rs
+++ src/deck.rs
@@ -1,14 +1,24 @@
-use crate::card::Card;
-//#[derive(Debug, Clone, Copy, PartialEq)]
+// import crates
+use crate::card::{Suit, Rank, Card};
+
+ // define Deck struct
  pub struct Deck {
-    deck: Vec<Card>,
+    pub cards: Vec<Card>,
  }
 
+ // define Deck implications
  impl Deck {
+    // constructor method
     pub fn new() -> Self {
-        Deck {
-            deck: Vec::new(),
+        // create vector to store cards
+        let mut cards = Vec::with_capacity(52);
+        // loop through suits and ranks to populate cards vector
+        for suit in Suit::all() {
+            for rank in Rank::all() {
+                cards.push(Card::new(suit, rank));
+            }
         }
+        // return cards as Deck using field init shorthand
+        Deck { cards }
     }
-    
  }
