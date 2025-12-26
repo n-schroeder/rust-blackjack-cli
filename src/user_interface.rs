a7bbbe080be1cce3134f842b8de55c5341656ec8
Noah Schroeder <noah.schroeder@gmail.com>
2025-12-26 11:08:00 -0700

    feat(UI): create function to get bet from user

--- src/user_interface.rs
+++ src/user_interface.rs
@@ -5,6 +5,7 @@
 
 // Imports
 use colored::Colorize;
+use std::io::{self};
 
 /// Clears screen and welcomes user based on iteration and bankroll
 pub fn display_header(i: u16, bankroll: u32) {
@@ -27,4 +28,46 @@ pub fn display_header(i: u16, bankroll: u32) {
     println!("{}", "│".cyan().bold());
     println!("{}", "└──────────────────────────────┘".cyan());
     println!();
+}
+
+/// Prompts user for bet, then obtains and validates the bet
+pub fn get_bet(bankroll:u32) -> u32 {
+    // start betting loop
+    loop {
+        // declare input var
+        let mut input: String = String::new();
+        let bet: u32;
+        // prompt player
+        print!("Enter your bet amount: $");
+        // real input
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read bet");
+
+        // attempt parse
+        bet = match input.trim().parse() {
+            // if result is good, return num
+            Ok(num) => num,
+            Err(_) => {
+                println!("That was not a number!");
+                continue
+            }
+        };
+
+        // verify valid bet
+        if bet > bankroll {
+            println!("Bad bet. Insufficient funds");
+            continue
+        }
+        if bet == 0 {
+            println!("You can't play for free ;)");
+            continue
+        }
+        if bet == 67 {
+            println!("chud");
+        }
+        
+        // exit betting loop
+        return bet;
+    }
 }
