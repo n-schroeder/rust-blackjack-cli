6b21459c345cbc1d11431ace4efcd386f1600b2f
Noah Schroeder <noah.schroeder@gmail.com>
2025-12-26 10:14:58 -0700

    feat(UI): create and document user interface module

---
+++ src/user-interface.rs
@@ -0,0 +1,4 @@
+//! User Interface
+//! 
+//! I will call methods from this module to prompt, gain information from, and display information to the user.
+//! This module is meant to process and validate inputs and pass it off.
