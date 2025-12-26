6ddeb0feddceb2d480e82efbaee1dd2dbfa0063a
Noah Schroeder <noah.schroeder@gmail.com>
2025-12-26 11:26:26 -0700

    feat(main): update game through the initial deal

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
-use crate::game::Game;
-
+// Declare existing files
 mod card;
 mod deck;
 mod hand;
 mod game;
 mod user_interface;
 
+// Imports
+use game::Game;
+use user_interface as ui;
+
 fn main() {
     // Create new game
-    let game = Game::new(1000);
+    let mut game = Game::new(1000);
 
     // Welcome user
-    user_interface::display_header(game.i, game.bankroll);
+    ui::display_header(game.i, game.bankroll);
+
+    // create temporary bet and assign to game.bet
+    let temp_bet: u32 = ui::get_bet(game.bankroll);
+    game.bet = temp_bet;
+
+    // initial deal
+    game.initial_deal();
 }
