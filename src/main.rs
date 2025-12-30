6f79c4b4d2b8fc6463a45606841e05b5d247185f
Noah Schroeder <noah.schroeder@gmail.com>
2025-12-30 09:52:28 -0700

    fix(main): create loops for player and dealer turns to simplify logic

--- src/main.rs
+++ src/main.rs
@@ -26,48 +26,49 @@ fn main() {
         game.initial_deal();
 
         // loop to handle player actions
-        loop {
+        'gameplay: loop {
             // show hands
             ui::show_hands(&game.player_hand, &game.dealer_hand);
 
             // Check for blackjacks
-            if game.player_hand.is_blackjack() { break }
+            if game.player_hand.is_blackjack() { break 'gameplay }
 
-            // check player bust
-            if game.player_bust() { break }
+            'player_turn: loop {
+                // check player bust
+                if game.player_bust() { break 'gameplay }
 
-            // Get user decision
-            if ui::player_hits() {
-                game.deal_to_player(); 
-                continue;
+                // Get user decision
+                if ui::player_hits() {
+                    game.deal_to_player(); 
+                    continue;
+                }
+                else { break 'player_turn }
             }
 
             // add dealer's downcard
             game.deal_to_dealer();
+            
+            // show dealer turn
+            println!("\n==== Dealer's Turn ====");
 
-            // show dealer hitting
-            println!("\n==== Dealer hits ====");
-            // show hands
-            ui::show_hands(&game.player_hand, &game.dealer_hand);
+            // dealer turn loop
+            loop {
+                // show hands
+                ui::show_hands(&game.player_hand, &game.dealer_hand);
 
-            // check for blackjack
-            if game.dealer_hand.is_blackjack() { break }
+                // check for blackjack
+                if game.dealer_hand.is_blackjack() { break 'gameplay }
 
-            // deal card to dealer
-            game.deal_to_dealer();
+                // check if >= 17
+                if game.dealer_hand.value() >= 17 { break 'gameplay }
 
-            // check bust
-            if game.dealer_bust() { break }
+                // deal card to dealer
+                println!("\n==== Dealer Hits ====");
+                game.deal_to_dealer();
 
-            // check if >= 17
-            if game.dealer_hand.value() >= 17 { break }
-        }
-
-        // win decision
-        // player bust
-        if game.player_bust() || game.dealer_hand.value() > game.player_hand.value() {
-            ui::print_outcome(false, game.bet);
-            game.bankroll -= game.bet;
+                // check bust
+                if game.dealer_bust() { break 'gameplay }
+            }
         }
 
         // prompt play again
